cfdaa9b5bad5b326768823865fea0716331a1bbf
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-04-19 18:03:18 -0500

    Integrando Redux

--- src/api/getPokemons.js
+++ src/api/getPokemons.js
@@ -1,3 +1,7 @@
-import axiosInstance from "./config";
+import axios from "../services/axios";
 
-export const getPokemons = (limit = 151) => axiosInstance.get(`/pokemon?limit=${limit}`);
+export const getPokemons = (limit = 151) =>
+    axios
+    .get(`/pokemon?limit=${limit}`)
+    .then((response) => response.data)
+    .catch((error) => console.log(error));
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,26 @@
 import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setPokemon } from '../../actions';
 import { getPokemons } from '../../api/getPokemons';
 import PokemonList from '../../components/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
-function Home() {
+const mapStateToProps = state => ({
+  list: state.list
+});
+
+const mapDispatchToProps = dispatch => ({
+  setPokemons: (value) => dispatch(setPokemon(value))
+});
+
+function Home({list, setPokemons}) {
   useEffect(() => {
-    getPokemons().then((res) => console.log(res));
-  })
+    getPokemons().then((res) => {
+        setPokemons(res.results);
+    });
+  }, []);
+
   return (
     <div className='Home'>
       <Searcher />
@@ -16,4 +29,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
